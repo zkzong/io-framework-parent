55c482e4da3d9a1247042f3ac0183a723afdfea6
admin <admin@admindeMacBook-Pro.local>
2021-08-02 11:13:14 +0800

    Initial commit

---
+++ src/main/java/com/zkzong/framework/FrameworkZongApplication.java
@@ -0,0 +1,13 @@
+package com.zkzong.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkZongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkZongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zkzong/framework/FrameworkZongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkzong.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkZongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
