1c4d92290ea58fad7a216545b04922d82944e8a2
zkzong <zongzhankui@hotmail.com>
2021-09-03 16:16:36 +0800

    api

---
+++ framework-core/src/main/java/com/zkzong/framework/core/config/ZWebMvcRegistrations.java
@@ -0,0 +1,16 @@
+package com.zkzong.framework.core.config;
+
+import com.zkzong.framework.core.annotation.impl.CustomRequestMappingHandlerMapping;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Configuration
+public class ZWebMvcRegistrations implements WebMvcRegistrations {
+
+    @Override
+    public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+        return new CustomRequestMappingHandlerMapping();
+    }
+
+}
