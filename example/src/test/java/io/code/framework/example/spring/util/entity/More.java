4abe3b0aa11827047d9014a8b5dd9a087bfc2b94
zkzong <zongzhankui@hotmail.com>
2024-12-19 22:56:37 +0800

    Spring Util

---
+++ example/src/test/java/io/code/framework/example/spring/util/entity/More.java
@@ -0,0 +1,20 @@
+package io.code.framework.example.spring.util.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by zong on 2017/2/24.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class More {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String sex;
+
+}
---
+++ example/src/test/java/io/code/framework/example/spring/util/entity/UserOne.java
@@ -0,0 +1,14 @@
+package io.code.framework.example.spring.util.entity;
+
+import lombok.Data;
+
+/**
+ * Created by Zong on 2017/5/22.
+ */
+@Data
+public class UserOne {
+
+    private int id;
+    private String name;
+
+}
