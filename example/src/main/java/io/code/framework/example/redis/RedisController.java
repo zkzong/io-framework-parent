043bbffb4207f41e877a32eeb19e1342f7119223
zkzong <zongzhankui@hotmail.com>
2024-12-28 12:05:08 +0800

    redis

--- example/src/main/java/io/code/framework/example/redis/RedisController.java
+++ example/src/main/java/io/code/framework/example/redis/RedisController.java
@@ -1,5 +1,9 @@
 package io.code.framework.example.redis;
 
+import io.code.framework.core.entity.ApiResponse;
+import io.code.framework.core.entity.ApiResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/redis")
 public class RedisController {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @RequestMapping("/get")
+    public ApiResponse<String> get() {
+        redisTemplate.opsForValue().set("redis", "Hello Redis");
+        String redis = redisTemplate.opsForValue().get("redis");
+        return ApiResponseUtil.success(redis);
+    }
 }
