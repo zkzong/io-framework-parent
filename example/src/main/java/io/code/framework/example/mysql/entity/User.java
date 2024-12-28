fdda27e0877bfbb642792ae40db8a3fa760192b9
zkzong <zongzhankui@hotmail.com>
2024-12-28 15:05:17 +0800

    mysql

--- example/src/main/java/io/code/framework/example/mysql/entity/User.java
+++ example/src/main/java/io/code/framework/example/mysql/entity/User.java
@@ -13,27 +13,17 @@ import java.io.Serializable;
 @TableName("t_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 747908897982690958L;
-    /**
-     * id
-     */
+
     @TableId(type = IdType.AUTO)
     private Integer id;
-    /**
-     * 姓名
-     */
+
     private String userName;
-    /**
-     * 年龄
-     */
+
     private Integer age;
-    /**
-     * 创建时间
-     */
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
-    /**
-     * 更新时间
-     */
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
