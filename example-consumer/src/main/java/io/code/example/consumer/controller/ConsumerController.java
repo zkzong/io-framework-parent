d0beb3859aede8f5bc69e1aa7f96828da67230d8
zkzong <zongzhankui@hotmail.com>
2025-01-09 21:57:16 +0800

    修改example

--- example-consumer/src/main/java/io/code/example/consumer/controller/ConsumerController.java
+++ example-consumer/src/main/java/io/code/example/consumer/controller/ConsumerController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,12 +26,6 @@ public class ConsumerController {
     public ApiResponse<UserVo> userFeign(@RequestBody UserDto userDto) {
         log.info("userDto = {}", userDto);
         return providerClient.user(userDto);
-    }
-
-    @PostMapping("/user/resttemplate")
-    public String userRest(@RequestBody UserDto userDto) {
-        log.info("userDto = {}", userDto);
-        return restTemplate.postForObject("http://127.0.0.1:8081/provider/user?name={name}", null, String.class, name);
     }
 
 }
