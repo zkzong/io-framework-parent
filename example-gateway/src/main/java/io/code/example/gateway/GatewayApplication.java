2b519ded9e7dfc05ccc150a019dcddfa2acbbf65
zkzong <zongzhankui@hotmail.com>
2025-01-09 23:15:21 +0800

    gateway

---
+++ example-gateway/src/main/java/io/code/example/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package io.code.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zongz
+ * @Date: 2025-01-06
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
