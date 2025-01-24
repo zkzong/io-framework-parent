c79feb64458cc9e49516d2e3e5bbdf88589973f2
zkzong <zongzhankui@hotmail.com>
2025-01-24 17:04:32 +0800

    gateway

---
+++ io-framework-gateway-server/src/main/java/io/code/framework/gateway/server/autoconfiguration/GatewayServerAutoConfiguration.java
@@ -0,0 +1,14 @@
+package io.code.framework.gateway.server.autoconfiguration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自动装配
+ * @Author: zongz
+ * @Date: 2025/01/24
+ */
+@Configuration
+@ComponentScan("io.code.framework.gateway.server")
+public class GatewayServerAutoConfiguration {
+}
