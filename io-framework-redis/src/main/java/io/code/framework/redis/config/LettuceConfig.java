071d9edd0f4c049fcd9f3497c3a97fab794c5e0a
zkzong <zongzhankui@hotmail.com>
2025-03-03 23:08:15 +0800

    lettuce

--- io-framework-redis/src/main/java/io/code/framework/redis/config/LettuceConfig.java
+++ io-framework-redis/src/main/java/io/code/framework/redis/config/LettuceConfig.java
@@ -1,10 +1,18 @@
 package io.code.framework.redis.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 /**
  * @Author: zongz
@@ -13,9 +21,60 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class LettuceConfig {
 
+    @Value("${spring.data.redis.database}")
+    private int database;
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.timeout}")
+    private long timeout;
+
+    @Value("${spring.data.redis.lettuce.shutdown-timeout}")
+    private long shutDownTimeout;
+
+    @Value("${spring.data.redis.lettuce.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.data.redis.lettuce.pool.min-idle}")
+    private int minIdle;
+
+    @Value("${spring.data.redis.lettuce.pool.max-active}")
+    private int maxActive;
+
+    @Value("${spring.data.redis.lettuce.pool.max-wait}")
+    private long maxWait;
+
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory();
+
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+        genericObjectPoolConfig.setMaxIdle(maxIdle);
+        genericObjectPoolConfig.setMinIdle(minIdle);
+        genericObjectPoolConfig.setMaxTotal(maxActive);
+        genericObjectPoolConfig.setMaxWaitMillis(maxWait);
+        genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(100);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setDatabase(database);
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+                .commandTimeout(Duration.ofMillis(timeout))
+                .shutdownTimeout(Duration.ofMillis(shutDownTimeout))
+                .poolConfig(genericObjectPoolConfig)
+                .build();
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
+        //factory.setShareNativeConnection(true);
+        //factory.setValidateConnection(false);
+        return factory;
     }
 
     /**
